34ba48a3194c75d874adf5159c7c6f3fa1ff0a0e
Aarchi-Ayoub <ayoubaa084@gmail.com>
2021-07-25 18:06:27 +0200

    Add & config react-native-config

--- android/app/src/main/java/com/food/MainApplication.java
+++ android/app/src/main/java/com/food/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+// adding for react-native-config module
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,6 +28,9 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          
+          // adding for react-native-config module
+          packages.add(new ReactNativeConfigPackage());
           return packages;
         }
 
